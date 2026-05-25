74b54de2bdb7825cbf2c10bff8b6e0b39d6c7648
MantaRay2 <jtimmer@calpoly.edu>
2026-05-24 20:56:19 -0700

    Further Card Setup
    Bunch o' changes

--- Assets/Scripts/CardScripts/Card.cs
+++ Assets/Scripts/CardScripts/Card.cs
@@ -1,14 +1,20 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace CardScripts
 {
+    //a way to make this card data in the unity editor
+    [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
     public class Card : ScriptableObject
     {
         public string cardName;
-        public CardType cardType;
-        public int health;
+        public List<CardType> cardType;
+        public Sprite cardSprite;
+        public int energyCost;
         public int damageMin;
         public int damageMax;
+        public int healthGain;
+        public int armorGain;
         //public DamageType damageType;
 
         public enum CardType
