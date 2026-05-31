6cd80cbc9fd6c06f869c7924b476186a9407ecf3
MantaRay2 <jtimmer@calpoly.edu>
2026-05-30 19:26:32 -0700

    Revert "Merge pull request #17 from jacobtimmer/Jacob's-Branch"
    This reverts commit 4bd88880f2848b5230bf0f93e824080300e5eb92, reversing
    changes made to 78184d5bc2bc8ca36af01b44ca41136d1a8977d9.

--- Assets/Scripts/CardScripts/CardDisplay.cs
+++ Assets/Scripts/CardScripts/CardDisplay.cs
@@ -17,26 +17,11 @@ public class CardDisplay : MonoBehaviour
 
     void Start()
     {
-        if (cardData != null)
-        {
-            UpdateCardDisplay(); //when this card initializes, it will update the display
-        }
-    }
-
-    public void Setup(Card newCard) //added so the HandManager can assign card data when drawing cards
-    {
-        cardData = newCard; //set this card display's data to the card that was drawn
-        UpdateCardDisplay(); //update the display after assigning the new card data
+        UpdateCardDisplay(); //when this card initializes, it will update the display
     }
 
     public void UpdateCardDisplay()
     {
-        if (cardData == null) //added so we don't get errors if cardData was not assigned yet
-        {
-            Debug.LogWarning("No card data assigned to this CardDisplay.");
-            return;
-        }
-
         //.text grabs the text component of the TMP_Text
         nameText.text = cardData.cardName;
         energyCostText.text = cardData.energyCost.ToString();
@@ -61,4 +46,5 @@ public class CardDisplay : MonoBehaviour
             }
         }
     }
+
 }
