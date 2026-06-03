3b7defc403c96c34c559ff3e58da2a2f34be4efb
MantaRay2 <jtimmer@calpoly.edu>
2026-06-02 21:51:58 -0700

    Temp Art Assets
    Temp Art Assets

--- Assets/Scripts/GameScripts/OptionsManager.cs
+++ Assets/Scripts/GameScripts/OptionsManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class OptionsManager : MonoBehaviour
 {
+    //can have something in here for changing the font used in the game  v11
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
